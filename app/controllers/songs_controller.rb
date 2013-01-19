21bae1c06d30bc08ce55bba86a39ff3fcc77fd72
Dcabrera007 <danielc00@live.com>
2013-01-18 22:42:05 -0500

    fixed

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,17 +1,83 @@
-require 'rubygems'
-gem 'soundcloud-ruby-api-wrapper'
-require 'soundcloud'
+class SongsController < ApplicationController
+  # GET /songs
+  # GET /songs.json
+  def index
+    @songs = Song.all
 
-# Create an anonymous soundcloud client
-# if you prefer to work with sandbox-soundcloud.com, replace this with:
-# sc_client = Soundcloud.register({:consumer_key => 'Your_Consumer_Key_Here', :site => "http://api.sandbox-soundcloud.com"})
-sc_client = Soundcloud.register
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @songs }
+    end
+  end
 
-# Find the 10 hottest tracks
-hot_tracks = sc_client.Track.find(:all,:params => {:order => 'hotness', :limit => 10})
+  # GET /songs/1
+  # GET /songs/1.json
+  def show
+    @song = Song.find(params[:id])
 
-# and display their titles
-p '==the 10 hottest tracks=='
-hot_tracks.each do |track|
-  p track.title
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @song }
+    end
+  end
+
+  # GET /songs/new
+  # GET /songs/new.json
+  def new
+    @song = Song.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @song }
+    end
+  end
+
+  # GET /songs/1/edit
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  # POST /songs
+  # POST /songs.json
+  def create
+    @song = Song.new(params[:song])
+
+    respond_to do |format|
+      if @song.save
+        format.html { redirect_to @song, notice: 'Song was successfully created.' }
+        format.json { render json: @song, status: :created, location: @song }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @song.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /songs/1
+  # PUT /songs/1.json
+  def update
+    @song = Song.find(params[:id])
+
+    respond_to do |format|
+      if @song.update_attributes(params[:song])
+        format.html { redirect_to @song, notice: 'Song was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @song.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /songs/1
+  # DELETE /songs/1.json
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+
+    respond_to do |format|
+      format.html { redirect_to songs_url }
+      format.json { head :no_content }
+    end
+  end
 end
