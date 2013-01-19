f8342ef2db11a2148fbfeca80e1d0b0899b565f7
Dcabrera007 <danielc00@live.com>
2013-01-18 21:45:31 -0500

    'initial'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JukeShop::Application.config.secret_token = 'f3317bf45e6b3d8b2cd00d00c6d7a9689d94b21339185f65432856678082618c73a4438a36971b294197c1e813ae9b8340d34120de95ea0d1f931af42150c19f'
